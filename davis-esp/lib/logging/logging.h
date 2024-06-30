110bfd7c02e4349f870fad6882cf36d9756e04d2
Daniel McMillan <mcmillan.daniel3@gmail.com>
2024-06-30 10:04:34 +1000

    Support format string in log macro

--- davis-esp/lib/logging/logging.h
+++ davis-esp/lib/logging/logging.h
@@ -2,11 +2,10 @@
 #define _LOGGING_H
 
 #ifndef DISABLE_LOGGING
-#define LOG_INFO(msg) Serial.println(F("INFO " msg))
-// #define LOG_ERROR(msg) Serial.println(F("ERROR " msg))
+#define LOG_INFO(msg, ...) Serial.printf("INFO " msg "\n" __VA_OPT__(, ) __VA_ARGS__)
 #define LOG_ERROR(msg, ...) Serial.printf("ERROR " msg "\n" __VA_OPT__(, ) __VA_ARGS__)
 #else
-#define LOG_INFO(msg)
+#define LOG_INFO(msg, ...)
 #define LOG_ERROR(msg, ...)
 #endif
 
